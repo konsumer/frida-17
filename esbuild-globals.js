5d2cbb43aeffceed081e1980acea58d3498c472e
David Konsumer <konsumer@jetboystudio.com>
2025-05-28 05:12:09 -0700

    esbuild example

---
+++ esbuild-globals.js
@@ -0,0 +1,4 @@
+// Export any global-vars here (for esbuild)
+
+export { default as ObjC } from 'frida-objc-bridge'
+export { default as Java } from 'frida-java-bridge'
---
+++ runner3.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+import { readFile } from 'node:fs/promises'
+import { basename } from 'node:path'
+import frida from 'frida'
+import * as esbuild from 'esbuild'
+
+const [, name, processName, scriptFile] = process.argv
+
+if (!processName || !scriptFile) {
+  console.error(`Usage: ${basename(name, '.js')} <PROCESS_NAME> <SCRIPTFILE>`)
+  process.exit(1)
+}
+
+const code = (
+  await esbuild.build({
+    write: false,
+    bundle: true,
+    entryPoints: [scriptFile],
+    inject: ['esbuild-globals.js'],
+    format: 'esm'
+  })
+)?.outputFiles[0].text
+
+const device = await frida.getUsbDevice()
+const session = await device.attach(processName)
+const script = await session.createScript(code)
+await script.load()
